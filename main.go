b028c5d50063cfdebd14b7d1c85d149b6f5c6e80
32b3 <32b3@protonmail.com>
2020-07-07 12:33:56 +0200

    static file server added

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 	"os"
 )
 
+var tpl = template.Must(template.ParseFiles("index.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, nil)
 }
 
 func main() {
@@ -16,6 +19,9 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
