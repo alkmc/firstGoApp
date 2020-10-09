0accb6a34473a12dd3daf2e8f942ab825a10751d
Alex <32b3@protonmail.com>
2020-10-09 20:41:29 +0200

    added file for env vars, gracefull shutdown implemented

---
+++ env.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	tpl    = template.Must(template.ParseFiles("index.html"))
+	apiKey *string
+)
+
+const (
+	readR   = 5 * time.Second   // max time to read request from the client
+	writeR  = 10 * time.Second  // max time to write response to the client
+	keepA   = 120 * time.Second // max time for connections using TCP Keep-Alive
+	timeout = 10 * time.Second  // max time to complete tasks before shutdown
+)
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":3000"
+		return port
+	}
+	return ":" + port
+}
+
+func checkAPIKey() {
+	apiKey = flag.String("apiKey", "", "Newsapi.org access key")
+	flag.Parse()
+
+	if *apiKey == "" {
+		log.Fatal("apiKey must be set")
+	}
+}
--- main.go
+++ main.go
@@ -1,37 +1,51 @@
 package main
 
 import (
-	"flag"
-	"html/template"
+	"context"
 	"log"
 	"net/http"
 	"os"
-)
-
-var (
-	tpl    = template.Must(template.ParseFiles("index.html"))
-	apiKey *string
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	apiKey = flag.String("apiKey", "", "Newsapi.org access key")
-	flag.Parse()
+	checkAPIKey()
 
-	if *apiKey == "" {
-		log.Fatal("apiKey must be set")
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-
+	port := getPort()
 	mux := http.NewServeMux()
+	s := http.Server{
+		Addr:         port,
+		Handler:      mux,
+		ReadTimeout:  readR,
+		WriteTimeout: writeR,
+		IdleTimeout:  keepA,
+	}
 
 	fs := http.FileServer(http.Dir("assets"))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	mux.HandleFunc("/search", searchHandler)
 	mux.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":"+port, mux)
+
+	log.Printf("Starting http server on port %s\n", port)
+	go func() {
+		if err := s.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Error starting server %s", err.Error())
+		}
+	}()
+	log.Print("Server Started")
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	<-done
+	log.Print("Signal closing server received")
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Println("server shutdown failed", "error", err)
+	}
+	log.Println("server shutdown gracefully")
 }
